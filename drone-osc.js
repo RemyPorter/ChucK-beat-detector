465923340bef3fce392c9b3ca25226649ba77fe6
Remy Porter <remyporter@jetpackshark.com>
2015-06-24 12:48:53 -0400

    Refactored the OSC paths to accomplish a few goals. First, every path now starts with the prefix "/drone" for all drone operations, which brings this in-line with more conventional OSC approaches. The next part of the path is now ID*n* where *n* is an integer identifying the drone. Again, this brings it back in line with more standard OSC approaches, but also means that we can easily control multiple drones from this script.

--- drone-osc.js
+++ drone-osc.js
@@ -4,11 +4,13 @@ var parseArgs = require("minimist");
 
 var port = 8081;
 var sources = "0.0.0.0";
+var id = 0;
 
 var argv = parseArgs(process.argv);
 
 if (argv.port) port = argv.port;
 if (argv.mask) sources = argv.mask;
+if (argv.id) id = argv.id;
 
 console.log("Launching OSC server on " + port +
 	", listening to addresses like " + sources);
@@ -28,39 +30,43 @@ function DroneControl() {
 	}
 }
 
+function path(id, path) {
+	return "/drone/ID" + id + path;
+}
+
 var srv = new osc.Server(port, sources);
 
 var dc = new DroneControl();
-dc.register("/test/op", function(drone, address, data) {
+dc.register(path(id, "/test/op"));, function(drone, address, data) {
 	console.log(data)
 })
-.register("/takeoff", function(drone, address, data) {
+.register(path(id, "/takeoff"));, function(drone, address, data) {
 	drone.takeoff(function() {
 		dc.flying = true;
 	});
 })
-.register("/land", function(drone, address, data) {
+.register(path(id, "/land"));, function(drone, address, data) {
 	drone.stop();
 	drone.land(function() {
 		dc.flying = false;
 	});
 })
-.register("/move/stop", function(drone, address, data) {
+.register(path(id, "/move/stop"));, function(drone, address, data) {
 	drone.stop();
 })
-.register("/config/outdoor", function(drone, address, data) {
+.register(path(id, "/config/outdoor"));, function(drone, address, data) {
 	drone.config("CONFIG:outdoor", data[0]);
 })
-.register("/config/vert_speed", function(drone, address, data) {
+.register(path(id, "/config/vert_speed"));, function(drone, address, data) {
 	drone.config("CONTROL:control_vz_max", data[0]);
 })
-.register("/config/yaw_speed", function(drone, address, data) {
+.register(path(id, "/config/yaw_speed"));, function(drone, address, data) {
 	drone.config("CONTROL:control_yaw", data[0]);
 })
-.register("/config/no_shell", function(drone, address, data) {
+.register(path(id, "/config/no_shell"));, function(drone, address, data) {
 	drone.config("CONTROL:flight_without_shell", data[0]);
 })
-.register("/reset", function(drone, address, data) {
+.register(path(id, "/reset"));, function(drone, address, data) {
 	drone.disableEmergency();
 	drone.stop();
 })
@@ -70,7 +76,7 @@ dc.register("/test/op", function(drone, address, data) {
 	"up", "down", 
 	"clockwise", "counterClockwise",
 	"front", "back"].forEach(function(direction) {
-		dc.register("/move/"+direction, function(drone, address, data) {
+		dc.register(path(id, "/move/"));+direction, function(drone, address, data) {
 			drone[direction](data[0]);
 		});
 	});
@@ -83,7 +89,7 @@ dc.register("/test/op", function(drone, address, data) {
 	'doublePhiThetaMixed', 'flipAhead', 
 	'flipBehind', 'flipLeft', 'flipRight']
 .forEach(function(anim) {
-	dc.register("/anim/" + anim, function(drone, address, data) {
+	dc.register(path(id, "/anim/")); + anim, function(drone, address, data) {
 		drone.animate(anim, data[0]);
 	})
 });
@@ -96,7 +102,7 @@ dc.register("/test/op", function(drone, address, data) {
 	'rearLeftGreenOthersRed', 'leftGreenRightRed', 
 	'leftRedRightGreen','blinkStandard']
 .forEach(function(light) {
-	dc.register("/light/" + light, function(drone, address, data) {
+	dc.register(path(id, "/light/")); + light, function(drone, address, data) {
 		drone.animateLeds(light, data[0], data[1]);
 	})
 });
--- drone.ck
+++ drone.ck
@@ -1,6 +1,7 @@
 public class Drone {
 	OscOut osc;
 
+	int id;
 	fun void outdoorMode() {
 		"true" => outdoor;
 		"true" => noShell;
@@ -8,61 +9,66 @@ public class Drone {
 		6.0 => yawSpeed;
 	}
 
-	fun void connect(string host, int port) {
+	fun string path(pathString) {
+		return "/drone/ID" + id + pathString;
+	}
+
+	fun void connect(string host, int port, int droneId) {
 		(host, port) => osc.dest;
+		droneId => id;
 	}
 	fun void takeoff() {
-		osc.start("/takeoff").send();
+		osc.start(path("/takeoff")).send();
 	}
 	fun void land() {
-		osc.start("/land").send();
+		osc.start(path("/land")).send();
 	}
 	fun void light(string pattern, float freq, float durInMills) {
-		osc.start("/light/" + pattern).add(freq).add(durInMills).send();
+		osc.start(path("/light/" + pattern)).add(freq).add(durInMills).send();
 	}
 	fun void anim(string pattern, float durInMills) {
-		osc.start("/anim/" + pattern).add(durInMills);
+		osc.start(path("/anim/" + pattern)).add(durInMills);
 	}
 	fun void forward(float speed) {
-		osc.start("/move/front").add(speed).send();
+		osc.start(path("/move/front")).add(speed).send();
 	}
 	fun void up(float speed) {
-		osc.start("/move/up").add(speed).send();
+		osc.start(path("/move/up")).add(speed).send();
 	}
 	fun void down(float speed) {
-		osc.start("/move/down").add(speed).send();
+		osc.start(path("/move/down")).add(speed).send();
 	}
 	fun void backward(float speed) {
-		osc.start("/move/back").add(speed).send();
+		osc.start(path("/move/back")).add(speed).send();
 	}
 	fun void left(float speed) {
-		osc.start("/move/left").add(speed).send();
+		osc.start(path("/move/left")).add(speed).send();
 	}
 	fun void right(float speed) {
-		osc.start("/move/right").add(speed).send();
+		osc.start(path("/move/right")).add(speed).send();
 	}
 	fun void clockwise(float speed) {
-		osc.start("/move/clockwise").add(speed).send();
+		osc.start(path("/move/clockwise")).add(speed).send();
 	}
 	fun void counterClockwise(float speed) {
-		osc.start("/move/counterClockwise").add(speed).send();	
+		osc.start(path("/move/counterClockwise")).add(speed).send();	
 	}
 	fun void stop() {
-		osc.start("/move/stop").send();
+		osc.start(path("/move/stop")).send();
 	}
 	fun void verticalSpeed(float speed) {
-		osc.start("/config/vert_speed").add(speed).send();
+		osc.start(path("/config/vert_speed")).add(speed).send();
 	}
 	fun void yawSpeed(float speed) {
-		osc.start("/config/yaw_speed").add(speed).send();
+		osc.start(path("/config/yaw_speed")).add(speed).send();
 	}
 	fun void outdoor(string val) {
-		osc.start("/config/outdoor").add(val).send();
+		osc.start(path("/config/outdoor")).add(val).send();
 	}
 	fun void noShell(string val) {
-		osc.start("/config/no_shell").add(val).send();
+		osc.start(path("/config/no_shell")).add(val).send();
 	}
 	fun void reset() {
-		osc.start("/reset").send();
+		osc.start(path("/reset")).send();
 	}
 }
